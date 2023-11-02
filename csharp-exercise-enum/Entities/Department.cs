c388557840977050ed9e299e831f2e22b128ed34
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-02 19:52:08 -0400

    feat: add departmet class

---
+++ csharp-exercise-enum/Entities/Department.cs
@@ -0,0 +1,12 @@
+﻿namespace csharp_exercise_enum.Entities
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
